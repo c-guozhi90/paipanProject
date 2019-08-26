c94a647a49a4f531be517a8e07e73af19db8b880
Raven Chan <c.guozhi90@gmail.com>
2019-08-26 23:04:05 +0800

    initialize project

---
+++ app/src/main/java/com/chenyue/allpaipan/calendar/Calender.java
@@ -0,0 +1,11 @@
+package com.chenyue.allpaipan.calendar;
+
+public class Calender {
+    // some methods
+
+    public static int calDayDiff(NewDate ealierDate, NewDate laterDate){
+        int days;
+        days=0;
+        return days;
+    }
+}
---
+++ app/src/main/java/com/chenyue/allpaipan/calendar/NewDate.java
@@ -0,0 +1,5 @@
+package com.chenyue.allpaipan.calendar;
+
+class NewDate {
+
+}
